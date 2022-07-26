6f2595af0b3e6472e8faf412c53812b05c95b2a5
Prathyusharam <prathyushaa1996@gmail.com>
2022-07-26 11:37:52 +0530

    feat: add channel images and css color variable

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import "./App.css";
+import "./App.css"
 import MockAPI from "./Mockman/Mockman";
 import { Routes, Route} from "react-router-dom";
-import { Navbar } from "./Components/index";
-import { VideoListing } from "./Pages/index";
+import { Navbar } from "./Components";
+import { SingleVideo, VideoListing } from "./Pages";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-      <Route path="/mockman" element={<MockAPI/>} />
-       <Route path="/Video" element={<VideoListing/>} /> 
+       <Route path="/mockman" element={<MockAPI />} />
+       <Route path="/Video" element={<VideoListing />} /> 
+       <Route path="/singlevideo/:videoID" element={<SingleVideo />} />
        </Routes>
       </div>
       
---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import ted from "../assets/images/ted-talks.png";
+import sanjeev from "../assets/images/img1.jpg";
+import hebbars from "../assets/images/hebbars.jpg";
+import sonymusic from "../assets/images/sonymusic.jpg";
+import viacom from "../assets/images/viacom.png";
+import HBOmax from "../assets/images/HBO-max.jpg";
+export {ted, sanjeev, hebbars, sonymusic, viacom, HBOmax}
